bd52356463edfb986b2ded8fbd468a8ca252a529
TemaXo <melnikov.artem294@gmail.com>
2026-04-26 13:29:19 +0300

    [feat] Created pinia store

---
+++ client/src/stores/user.store.ts
@@ -0,0 +1,33 @@
+import {defineStore} from "pinia";
+import {onMounted, ref} from "vue";
+
+export const useUserStore = defineStore('user', () => {
+  const user_id = ref<string | null>(null);
+
+  const setUser = (id: string): void => {
+    user_id.value = id;
+    localStorage.setItem('user', id)
+  }
+
+  const removeUser = (id: string): void => {
+    localStorage.removeItem('user');
+  }
+
+  const getUser = (): void => {
+    const user = localStorage.getItem('user')
+    if (user) {
+      user_id.value = user
+    }
+  }
+
+  onMounted(() => {
+    getUser()
+  })
+
+  return {
+    user_id,
+    setUser,
+    removeUser,
+    getUser
+  }
+})
